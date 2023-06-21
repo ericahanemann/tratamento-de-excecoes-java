9eba36a6a5530b44c0921e249b561b700519883a
Érica Hanemann <ericavhanemann@gmail.com>
2023-06-21 15:44:17 -0300

    try, catch e finally exemplo

--- teste-exemplos/src/TryCatchFinally.java
+++ teste-exemplos/src/TryCatchFinally.java
@@ -1,2 +1,20 @@
 public class TryCatchFinally {
+    public static void main(String[] args) {
+        String frase = null;
+        String novaFrase = null;
+
+        try {
+            novaFrase = frase.toUpperCase();
+        }
+        catch(NullPointerException e) {
+            System.out.println("\nA frase inicial é nula. Para solucionar o problema, " +
+                    "atribuímos um valor default para a frase.");
+            frase = "Frase vazia";
+        }
+        finally {
+            novaFrase = frase.toUpperCase();
+            System.out.println("Frase antiga: "+ frase);
+            System.out.println("Frase nova: "+ novaFrase);
+        }
+    }
 }
