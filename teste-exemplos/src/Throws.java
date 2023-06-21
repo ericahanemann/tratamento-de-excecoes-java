470e8fd0093daa618888898fced04055ff293087
mateus <mateus09022004@gmail.com>
2023-06-21 16:12:32 -0300

    commit do throws

--- teste-exemplos/src/Throws.java
+++ teste-exemplos/src/Throws.java
@@ -1,2 +1,14 @@
 public class Throws {
+    static void checkIdade(int idade) throws ArithmeticException {
+        if (idade < 18) {
+            throw new ArithmeticException("Acesso negado - você precisa ter no minimo 18 anos de idade!");
+        }
+        else {
+            System.out.println("Acesso concedido - você tem idade o suficiente");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkIdade(15); // Set age to 15 (which is below 18...)
+    }
 }
