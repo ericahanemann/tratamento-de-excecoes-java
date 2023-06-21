2244b11c2862c533a908a9d284a60fe2beddfa8a
Érica Hanemann <ericavhanemann@gmail.com>
2023-06-21 13:40:27 -0300

    organizando arquivos

--- teste-exemplos/src/Throw.java
+++ teste-exemplos/src/Throw.java
@@ -1,10 +1,14 @@
-public class Main {
+public class Throw {
 
     public static int dividir(int dividendo, int divisor) {
         if (divisor == 0) {
             throw new ArithmeticException("Divisão por zero não é permitida.");
         }
         return dividendo / divisor;
+    }
+
+    public static void main(String[] args) {
+        dividir(100, 0);
     }
 
     }
